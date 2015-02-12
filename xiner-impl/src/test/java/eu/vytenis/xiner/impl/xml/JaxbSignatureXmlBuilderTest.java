05cfbca91e14f0d4b3b89082a1092dbfd67b058a
Vytenis Bivainis <monrealis@gmail.com>
2015-02-12 23:26:01 +0200

    Parameterize JaxbSignatureXmlBuilderTest.

--- xiner-impl/src/test/java/eu/vytenis/xiner/impl/xml/JaxbSignatureXmlBuilderTest.java
+++ xiner-impl/src/test/java/eu/vytenis/xiner/impl/xml/JaxbSignatureXmlBuilderTest.java
@@ -2,20 +2,35 @@ package eu.vytenis.xiner.impl.xml;
 
 import eu.vytenis.xiner.api.Keys;
 import eu.vytenis.xiner.api.SignatureXmlBuilder;
+import eu.vytenis.xiner.api.XmlSignature;
 import eu.vytenis.xiner.impl.keys.Pkcs12Keys;
+import eu.vytenis.xiner.impl.xmldsig.JdkXmlSignature;
+import eu.vytenis.xiner.impl.xmldsig.SantuarioXmlSignature;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.w3c.dom.Document;
 
 import javax.xml.crypto.MarshalException;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
 import javax.xml.crypto.dsig.dom.DOMValidateContext;
+import java.util.List;
 
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
+import static org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class JaxbSignatureXmlBuilderTest {
     private SignatureXmlBuilder builder = new JaxbSignatureXmlBuilder();
     private Keys keys = new Pkcs12Keys();
+    public XmlSignature signature;
+
+    public JaxbSignatureXmlBuilderTest(XmlSignature signature) {
+        this.signature = signature;
+    }
 
     @Test
     public void shouldContainSignature() {
@@ -27,6 +42,11 @@ public class JaxbSignatureXmlBuilderTest {
         Document d = builder.createSignatureDocument();
         DOMValidateContext c = new DOMValidateContext(keys.getKey().getPublic(), d.getDocumentElement());
         XMLSignatureFactory.getInstance().unmarshalXMLSignature(c);
+    }
 
+    @Parameters
+    public static List<Object[]> getParams() {
+        XmlSignature[] signatures = {new JdkXmlSignature(), new SantuarioXmlSignature()};
+        return stream(signatures).map(o -> new Object[]{o}).collect(toList());
     }
 }
